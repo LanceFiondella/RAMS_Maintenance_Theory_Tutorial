a0da62eaece11bb1a94f30d52ec9a94c96c2fd63
Maskura Nafreen <34749177+Nickk1995@users.noreply.github.com>
2019-07-23 16:16:04 -0400

    Add files via upload

---
+++ MLE of Weibull.R
@@ -0,0 +1,63 @@
+### Maximum Likelihood Estimates for the Weibull Distribution
+library(plotly)
+set.seed(1234)
+
+alpha = 1.5   # shape
+beta = 1    # scale	
+
+n = 50
+
+y = rweibull(n, alpha, beta)
+
+# plot the simulated data
+
+plot(density(y))
+rug(y)
+
+# minimize logL
+
+logL = function(x){
+      -sum(dweibull(y, shape = x[1], scale = x[2], log = TRUE))
+}
+
+y.mle = nlm(logL, c(shape = 1.5, scale = 1), hessian = TRUE)
+y.mle
+
+# pdf and cdf plots fitted weibull model
+plot(ecdf(y))
+par(new=TRUE)
+plot(function(y) pweibull(y, shape = y.mle$estimate[1], 
+      scale = y.mle$estimate[2]), 0, max(y), xlab = "lifetimes", 
+      ylab = "density", main = "fitted Weibull density using MLE")
+
+hist(y)
+par(new=TRUE)
+plot(function(y) dweibull(y, shape = y.mle$estimate[1], 
+      scale = y.mle$estimate[2]), 0, max(y), xlab = "lifetimes", 
+      ylab = "density", main = "fitted Weibull density using MLE")
+
+rug(y)
+# contour plot
+
+len = 101
+
+grid = matrix(0.0, nrow = len, ncol = len)
+shape.vals = seq(0.01,3.5, len = len)
+scale.vals = seq(0.01, 3.5, len = len)
+
+for(i in seq(along = shape.vals)){
+      for(j in seq(along = scale.vals)){
+            grid[i,j] = logL(c(shape.vals[i],scale.vals[j]))
+      }
+}
+contour(shape.vals, scale.vals, grid, levels = 50:100)
+points(y.mle$estimate[1],y.mle$estimate[2], pch = "+")
+title(main = "log-Likelihood contours, Weibull", xlab = expression(alpha), 
+      ylab = expression(beta))
+
+
+
+
+
+
+
